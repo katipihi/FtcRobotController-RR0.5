d466a4f1b360cce958a1e1b2d4d12fffe39ffb5d
Robotica CAD3 <rogiergithub@gmail.com>
2023-11-22 14:47:56 +0100

    auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/StandardTrackingWheelLocalizer.java
@@ -51,9 +51,9 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
         lastEncPositions = lastTrackingEncPositions;
         lastEncVels = lastTrackingEncVels;
 
-        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, ConfigurationName.leftRear));
-        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, ConfigurationName.leftFront));
-        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, ConfigurationName.rightRear));
+        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, ConfigurationName.rightRear));
+        rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, ConfigurationName.leftRear));
+        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, ConfigurationName.leftFront));
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
         leftEncoder.setDirection(Encoder.Direction.REVERSE);
